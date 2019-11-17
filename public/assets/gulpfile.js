5d37a2a06fea59fbdc7f12d05e5251f591a438d6
Pavel Chernyshev <vengador32@gmail.com>
2019-11-17 15:59:44 +0600

    added styles

--- public/assets/gulpfile.js
+++ public/assets/gulpfile.js
@@ -15,7 +15,7 @@ var gulp         = require('gulp'),
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
-			baseDir: 'public/assets'
+			baseDir: '.'
 		},
 		notify: false,
 		// online: false, // Work offline without internet connection
@@ -26,7 +26,7 @@ function bsReload(done) { browserSync.reload(); done(); };
 
 // Custom Styles
 gulp.task('styles', function() {
-	return gulp.src('public/assets/sass/**/*.sass')
+	return gulp.src('sass/**/*.sass')
 	.pipe(sass({
 		outputStyle: 'expanded',
 		includePaths: [__dirname + '/node_modules']
@@ -37,7 +37,7 @@ gulp.task('styles', function() {
 		overrideBrowserslist: ['last 10 versions']
 	}))
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
-	.pipe(gulp.dest('public/assets/css'))
+	.pipe(gulp.dest('css'))
 	.pipe(browserSync.stream())
 });
 
@@ -45,12 +45,12 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
 	return gulp.src([
 		'node_modules/jquery/dist/jquery.min.js', // Optional jQuery plug-in (npm i --save-dev jquery)
-		'public/assets/js/_libs.js', // JS libraries (all in one)
-		'public/assets/js/_custom.js', // Custom scripts. Always at the end
+		'js/_libs.js', // JS libraries (all in one)
+		'js/_custom.js', // Custom scripts. Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
 	.pipe(uglify()) // Minify js (opt.)
-	.pipe(gulp.dest('public/assets/js'))
+	.pipe(gulp.dest('js'))
 	.pipe(browserSync.reload({ stream: true }))
 });
 
@@ -59,42 +59,42 @@ var quality = 95; // Responsive images quality
 
 // Produce @1x images
 gulp.task('img-responsive-1x', async function() {
-	return gulp.src('public/assets/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
-		.pipe(newer('public/assets/img/@1x'))
+	return gulp.src('img/_src/**/*.{png,jpg,jpeg,webp,raw}')
+		.pipe(newer('img/@1x'))
 		.pipe(responsive({
 			'**/*': { width: '50%', quality: quality }
 		})).on('error', function (e) { console.log(e) })
 		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
-		.pipe(gulp.dest('public/assets/img/@1x'))
+		.pipe(gulp.dest('img/@1x'))
 });
 // Produce @2x images
 gulp.task('img-responsive-2x', async function() {
-	return gulp.src('public/assets/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
-		.pipe(newer('public/assets/img/@2x'))
+	return gulp.src('img/_src/**/*.{png,jpg,jpeg,webp,raw}')
+		.pipe(newer('img/@2x'))
 		.pipe(responsive({
 			'**/*': { width: '100%', quality: quality }
 		})).on('error', function (e) { console.log(e) })
 		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
-		.pipe(gulp.dest('public/assets/img/@2x'))
+		.pipe(gulp.dest('img/@2x'))
 });
 gulp.task('img', gulp.series('img-responsive-1x', 'img-responsive-2x', bsReload));
 
 // Clean @*x IMG's
 gulp.task('cleanimg', function() {
-	return del(['public/assets/img/@*'], { force: true })
+	return del(['img/@*'], { force: true })
 });
 
 // Code & Reload
 gulp.task('code', function() {
-	return gulp.src('public/assets/**/*.html')
+	return gulp.src('**/*.html')
 	.pipe(browserSync.reload({ stream: true }))
 });
 
 // Deploy
 gulp.task('rsync', function() {
-	return gulp.src('public/assets/')
+	return gulp.src('')
 	.pipe(rsync({
-		root: 'public/assets/',
+		root: '',
 		hostname: 'username@yousite.com',
 		destination: 'yousite/public_html/',
 		// include: ['*.htaccess'], // Included files
@@ -107,10 +107,10 @@ gulp.task('rsync', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch('public/assets/sass/**/*.sass', gulp.parallel('styles'));
-	gulp.watch(['public/assets/js/_custom.js', 'public/assets/js/_libs.js'], gulp.parallel('scripts'));
-	gulp.watch('public/assets/*.html', gulp.parallel('code'));
-	gulp.watch('public/assets/img/_src/**/*', gulp.parallel('img'));
+	gulp.watch('sass/**/*.sass', gulp.parallel('styles'));
+	gulp.watch(['js/_custom.js', 'js/_libs.js'], gulp.parallel('scripts'));
+	gulp.watch('*.html', gulp.parallel('code'));
+	gulp.watch('img/_src/**/*', gulp.parallel('img'));
 });
 
 gulp.task('default', gulp.parallel('img', 'styles', 'scripts', 'browser-sync', 'watch'));
